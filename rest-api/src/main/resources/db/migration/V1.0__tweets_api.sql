112541162e6d88afc63bc8faca6cfc0f9c2842f8
sebastian.schmidt <bastman77@gmail.com>
2018-03-07 06:34:49 +0100

    tweets-api: playing with custom postgres types

--- rest-api/src/main/resources/db/migration/V1.0__tweets_api.sql
+++ rest-api/src/main/resources/db/migration/V1.0__tweets_api.sql
@@ -1,12 +1,17 @@
-CREATE TABLE tweet (
+CREATE TYPE TweetStatusType AS ENUM ('DRAFT', 'PENDING', 'PUBLISHED');
+
+CREATE TABLE Tweet (
   id         UUID                        NOT NULL,
   version    INTEGER                     NOT NULL,
   created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
   updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  message    TEXT                        NOT NULL,
+  deleted_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT TIMESTAMP 'epoch',
+
+  status TweetStatusType NOT NULL DEFAULT 'DRAFT',
+  message    CHARACTER VARYING(255) NOT NULL,
   comment    TEXT                        NULL
 );
-ALTER TABLE tweet
+ALTER TABLE Tweet
   OWNER TO app_rw;
-ALTER TABLE ONLY tweet
+ALTER TABLE ONLY Tweet
   ADD CONSTRAINT tweet_pkey PRIMARY KEY (id);

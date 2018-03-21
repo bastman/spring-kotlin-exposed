5a57178d70fed8352f0820931ab516898d6ca947
sebastian.schmidt <bastman77@gmail.com>
2018-03-21 08:05:55 +0100

    - enum colum type: more flexible

--- rest-api/src/main/kotlin/com/example/api/tweeter/db/tweetsTable.kt
+++ rest-api/src/main/kotlin/com/example/api/tweeter/db/tweetsTable.kt
@@ -16,11 +16,14 @@ object TweetsTable : Table("tweet") {
     val message = varchar("message", 255)
     val comment = text("comment").nullable()
     val status = enumerationByNameAndSqlType(
-            name = "status", sqlType = "TweetStatusType", klass = TweetStatus::class.java
+            name = "status", sqlType = "TweetStatusType",
+            klass = TweetStatus::class.java,
+            serialize = {toDb -> (toDb as TweetStatus).dbValue},
+            unserialize = {fromDb: String -> TweetStatus.values().first { it.dbValue==fromDb }; }
     ).default(TweetStatus.DRAFT)
 }
 
-enum class TweetStatus { DRAFT, PENDING, PUBLISHED; }
+enum class TweetStatus(val dbValue:String) { DRAFT("DRAFT"), PENDING("PENDING"), PUBLISHED("PUBLISHED"); }
 
 data class TweetsRecord(
         val id: UUID, val createdAt: Instant, val modifiedAt: Instant, val deletedAt: Instant, val version: Int,
--- rest-api/src/main/kotlin/com/example/util/exposed/columnTypes/enumByTypeColumnType.kt
+++ rest-api/src/main/kotlin/com/example/util/exposed/columnTypes/enumByTypeColumnType.kt
@@ -6,24 +6,42 @@ import org.jetbrains.exposed.sql.Table
 import org.postgresql.util.PGobject
 import java.sql.PreparedStatement
 
-fun <T : Any> Table.enumerationByNameAndSqlType(name: String, sqlType: String, klass: Class<T>): Column<T> =
-        registerColumn(name, EnumBySqlType(sqlType, klass))
+fun <T : Enum<T>> Table.enumerationByNameAndSqlType(
+        name: String,
+        sqlType: String,
+        klass: Class<T>,
+        serialize: (toDb: Enum<*>)->String = { it.name },
+        unserialize: (fromDb:String)->Enum<*> = {fromDb-> klass.enumConstants!!.first { it.name == fromDb }}
+): Column<T> =
+        registerColumn(name,
+                EnumBySqlType(sqlType, klass, serialize, unserialize)
+        )
 
-private class EnumBySqlType<out T : Any>(
+private class EnumBySqlType<T : Enum<T>>(
         private val sqlType: String,
-        private val klass: Class<T>
+        private val klass: Class<T>,
+        private val serialize: (toDb: Enum<*>)->String,
+        private val unserialize: (fromDb:String)->Enum<*>
 ) : ColumnType() {
     override fun sqlType() = sqlType
 
-
     override fun notNullValueToDB(value: Any): Any = when (value) {
-        is Enum<*> -> value.name
+       // is String -> value
+        is Enum<*> -> try {
+            serialize(value)
+        } catch (all:Exception) {
+            error("$value of ${value::class.qualifiedName} is not valid for enum ${klass.name} . details: ${all.message}")
+        }
         else -> error("$value of ${value::class.qualifiedName} is not valid for enum ${klass.name}")
     }
 
     override fun valueFromDB(value: Any): Any = when (value) {
-    //is Number -> klass.enumConstants!![value.toInt()]
-        is Enum<*> -> value
+        is String -> try {
+            unserialize(value)
+        }catch (all:Exception) {
+            error("$value of ${value::class.qualifiedName} is not valid for enum ${klass.name} . details: ${all.message}")
+        }
+        //is Enum<*> -> value
         else -> error("$value of ${value::class.qualifiedName} is not valid for enum ${klass.name}")
     }
 

6925c1e69e37920b32102cf1125e638629052785
sebastian.schmidt <bastman77@gmail.com>
2019-10-17 10:21:10 +0200

    poc: pq "SELECT DISTINCT ON (a,b,c,...)"

---
+++ rest-api/src/main/kotlin/com/example/util/exposed/expr/DistinctOn.kt
@@ -0,0 +1,17 @@
+package com.example.util.exposed.expr
+
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Expression
+import org.jetbrains.exposed.sql.IntegerColumnType
+import org.jetbrains.exposed.sql.QueryBuilder
+
+// see: https://github.com/JetBrains/Exposed/issues/500
+
+/*
+fun Column<*>.distinctOn(): Function<Int> = DistinctOn(this)
+
+class DistinctOn(val expr: Expression<*>) : Function<Int>(IntegerColumnType()) {
+    override fun toSQL(queryBuilder: QueryBuilder) = "DISTINCT ON (${expr.toSQL(queryBuilder)}) ${expr.toSQL(queryBuilder)}"
+}
+
+ */

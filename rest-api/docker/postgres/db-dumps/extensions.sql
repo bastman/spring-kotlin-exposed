aa29910671ce2b46ab8255da2a74332614f793f5
sebastian.schmidt <bastman77@gmail.com>
2019-10-21 11:59:12 +0200

    gis example: places api - dbmodel

---
+++ rest-api/docker/postgres/db-dumps/extensions.sql
@@ -0,0 +1,4 @@
+-- custom extensions ---
+CREATE EXTENSION IF NOT EXISTS cube;
+CREATE EXTENSION IF NOT EXISTS earthdistance;
+
---
+++ rest-api/src/main/resources/db/migration/V5.0__gis_places_api.sql
@@ -0,0 +1,22 @@
+
+-- table: place (gis example)
+CREATE TABLE place
+(
+    place_id                     uuid               NOT NULL,
+    created_at                   timestamp          NOT NULL,
+    modified_at                  timestamp          NOT NULL,
+    deleted_at                   timestamp          NULL,
+    active                       bool               NOT NULL,
+    country_name                 varchar(2048)      NOT NULL,
+    city_name                    varchar(2048)      NOT NULL,
+    postal_code                  varchar(2048)      NOT NULL,
+    street_address               varchar(2048)      NOT NULL,
+    formatted_address            varchar(2048)      NOT NULL,
+    latitude                     numeric(10, 6)     NOT NULL,
+    longitude                    numeric(10, 6)     NOT NULL,
+
+    CONSTRAINT place_pkey PRIMARY KEY (place_id)
+);
+
+-- table: place_geosearch_index -> create gist-index:
+CREATE INDEX place_geosearch_index ON place USING gist (ll_to_earth(latitude, longitude));

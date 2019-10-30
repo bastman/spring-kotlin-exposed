7a5501ed6a8c50412057113e35d1dfbf6fc92146
sebastian.schmidt <bastman77@gmail.com>
2019-10-30 10:43:42 +0100

    earth()

--- rest-api/src/main/kotlin/com/example/util/exposed/postgres/extensions/earthdistance/EarthDistance.kt
+++ rest-api/src/main/kotlin/com/example/util/exposed/postgres/extensions/earthdistance/EarthDistance.kt
@@ -3,8 +3,15 @@ package com.example.util.exposed.postgres.extensions.earthdistance
 import org.jetbrains.exposed.sql.CustomFunction
 import org.jetbrains.exposed.sql.DoubleColumnType
 
-// select earth() -> returns "Earth as float8" ( SELECT '6378168'::float8 )
+// select earth() -> returns the assumed radius of te earth as float8 ( SELECT '6378168'::float8 )
 fun earth(): CustomFunction<Double> {
     val fn = CustomFunction<Double>("earth", DoubleColumnType())
     return fn
 }
+
+
+/**
+
+select ll_to_earth( 11.1 , 20.0 ); -> returns (5881394.65979286, 2140652.5921368, 1227937.44619261)
+select latitude('(5881394.65979286, 2140652.5921368, 1227937.44619261)'::earth); -> returns 11.1
+ */

606ee421ebae55b11a92c369f0a6d1a3198b6b6d
sebastian.schmidt <bastman77@gmail.com>
2019-11-01 09:30:52 +0100

    poc

--- rest-api/src/main/kotlin/com/example/util/exposed/postgres/extensions/earthdistance/PGEarthPointLocationTypes.kt
+++ rest-api/src/main/kotlin/com/example/util/exposed/postgres/extensions/earthdistance/PGEarthPointLocationTypes.kt
@@ -7,16 +7,20 @@ import java.sql.PreparedStatement
 
 data class PGEarthPointLocation(val x:Double, val y:Double, val z:Double)
 class PGEarthPointLocationColumnType: ColumnType() {
-    override fun sqlType(): String  = "cube"
+    private val pgObjectType:String =  "cube"
+    override fun sqlType(): String  = pgObjectType
+
+    private fun PGEarthPointLocation.toPgValue():String =  "($x, $y, $z)"
+
 
     override fun valueFromDB(value: Any): PGEarthPointLocation {
-        var pgType:String?=null
-        var pgValue:String?=null
+        var pgTypeGiven:String?=null
+        var pgValueGiven:String?=null
         return try {
             value as PGobject
-            pgType=value.type
-            pgValue=value.value
-            val t = PGtokenizer(PGtokenizer.removePara(pgValue), ',')
+            pgTypeGiven=value.type
+            pgValueGiven=value.value
+            val t = PGtokenizer(PGtokenizer.removePara(pgValueGiven), ',')
             PGEarthPointLocation(
                     x=java.lang.Double.parseDouble(t.getToken(0)),
                     y = java.lang.Double.parseDouble(t.getToken(1)),
@@ -24,7 +28,7 @@ class PGEarthPointLocationColumnType: ColumnType() {
             )
         } catch (e: NumberFormatException) {
             throw PSQLException(
-                    GT.tr("Conversion to type $pgType -> ${this.javaClass} failed: $pgValue."),
+                    GT.tr("Conversion to type $pgTypeGiven -> ${this::class.qualifiedName} failed: $pgValueGiven."),
                     PSQLState.DATA_TYPE_MISMATCH, e)
         }
     }
@@ -35,8 +39,7 @@ class PGEarthPointLocationColumnType: ColumnType() {
         obj.value = when(value) {
             null->null
             else-> try {
-                value as PGEarthPointLocation
-                "(${value.x}, ${value.y}, ${value.z})"
+                (value as PGEarthPointLocation).toPgValue()
             } catch (all:Exception) {
                 throw PSQLException(
                         "Failed to setParameter at index: $index - value: $value ! reason: ${all.message}",
@@ -50,17 +53,9 @@ class PGEarthPointLocationColumnType: ColumnType() {
 
     override fun notNullValueToDB(value: Any): PGEarthPointLocation {
         return value as PGEarthPointLocation
-        /*
-        return when(value) {
-            is PGEarthPointLocation -> value
-            else -> super.notNullValueToDB(value)
-        }
-
-         */
     }
     override fun nonNullValueToString(value: Any): String {
-        val sinkValue = notNullValueToDB(value)
-        // "(${value.x}, ${value.y}, ${value.z})"
-        return "'(${sinkValue.x}, ${sinkValue.y}, ${sinkValue.z})'"
+        val sinkValue:PGEarthPointLocation = notNullValueToDB(value)
+        return "'${sinkValue.toPgValue()}'"
     }
 }

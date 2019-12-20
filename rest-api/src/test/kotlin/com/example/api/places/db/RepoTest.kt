3960cf80bd3ffb381c9641f38ba9e71760e38230
sebastian.schmidt <bastman77@gmail.com>
2019-12-20 09:40:25 +0100

    add tests

--- rest-api/src/test/kotlin/com/example/api/places/db/RepoTest.kt
+++ rest-api/src/test/kotlin/com/example/api/places/db/RepoTest.kt
@@ -4,10 +4,13 @@ import com.example.api.common.rest.error.exception.EntityNotFoundException
 import com.example.api.places.common.db.PlaceRecord
 import com.example.api.places.common.db.PlaceRepo
 import com.example.api.places.fixtures.PlacesApiFixtures
+import com.example.api.places.fixtures.randomized
 import com.example.testutils.assertions.shouldEqualRecursively
+import com.example.testutils.minutest.minuTestFactory
 import com.example.testutils.spring.BootWebMockMvcTest
 import org.amshove.kluent.shouldBe
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import java.time.Instant
@@ -78,5 +81,69 @@ class PlaceRepoTest(
                     assertThrows<EntityNotFoundException> { repo.getById(placeId = recordId, isActive = isActive) }
                 }
     }
+
+
+    @TestFactory
+    fun `some random crud ops should work`() = minuTestFactory {
+        val testCases: List<TestCase> = (0..100).map {
+            val recordNew: PlaceRecord = PlacesApiFixtures
+                    .newPlaceRecord()
+                    .randomized(preserveIds = true, preserveIsActive = true)
+
+            TestCase(
+                    recordNew = recordNew,
+                    recordsUpdate = (0..10).map {
+                        recordNew.randomized(preserveIds = true, preserveIsActive = true)
+                    }
+            )
+        }
+        context("prepare ...") {
+            testCases.onEach { testCase ->
+                context("prepare: ${testCase.recordNew}") {
+                    testCase.recordNew shouldEqualRecursively testCase.recordNew
+                    testCase shouldEqualRecursively testCase
+                }
+            }
+        }
+
+        testCases.forEachIndexed { testCaseIndex, testCase ->
+            context("test ($testCaseIndex): ${testCase.recordNew}") {
+
+                context("DB INSERT") {
+                    test("INSERT: ${testCase.recordNew}") {
+                        repo.insert(testCase.recordNew)
+                                .also { it shouldEqualRecursively testCase.recordNew }
+                    }
+                    test("GET INSERTED: ${testCase.recordNew}") {
+                        repo.getById(placeId = testCase.recordNew.place_id, isActive = true)
+                                .also { it shouldEqualRecursively testCase.recordNew }
+                    }
+                }
+                context("DB UPDATE") {
+                    testCase.recordsUpdate.forEachIndexed { index, recordToUpdate ->
+                        context("DB UPDATE ($index): $recordToUpdate") {
+                            test("UPDATE ($index): $recordToUpdate") {
+                                repo.update(recordToUpdate)
+                                        .also { it shouldEqualRecursively recordToUpdate }
+                            }
+                            test("GET UPDATED ($index): $recordToUpdate") {
+                                repo.getById(placeId = recordToUpdate.place_id, isActive = true)
+                                        .also { it shouldEqualRecursively recordToUpdate }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+
+    private data class TestCase(
+            val recordNew: PlaceRecord,
+            val recordsUpdate: List<PlaceRecord>
+    )
 
 }

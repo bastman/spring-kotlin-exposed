9823d37be53c6ca3e89fc1fa8478bb15bd6469d6
sebastian.schmidt <bastman77@gmail.com>
2020-10-28 16:00:02 +0100

    owasp.dependencycheck:6.0.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
         kotlin("jvm") version kotlinVersion
         id("tanvd.kosogor") version "1.0.7"
         id("io.gitlab.arturbosch.detekt") version "1.14.2"
-        id("org.owasp.dependencycheck") version "5.3.2.1"
+        id("org.owasp.dependencycheck") version "6.0.2"
         id("com.avast.gradle.docker-compose") version "0.13.4"
         id("com.github.ben-manes.versions") version "0.33.0"
         id("org.jetbrains.dokka") version "1.4.10.2"
